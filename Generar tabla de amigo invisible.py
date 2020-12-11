a1fb8f62df0254897e30e39a2b418f2c58c2970a
maryskal <71440659+maryskal@users.noreply.github.com>
2020-12-11 23:16:48 +0100

    Add files via upload

---
+++ Generar tabla de amigo invisible.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 11 21:19:15 2020
+
+@author: Maria Rollan
+"""
+
+import random
+import numpy as np
+import pandas as pd
+
+
+nombres = ['Inés', 'María', 'Clara', 'Mari', 'Fer', 'Ana', 'Nacho', 'Belén', 'Agustín', 'Mina']
+regalos = []
+repetido = False
+
+
+for x in nombres:
+    print('\n \n', x)
+    
+    #Generamos un número aleatorio de la longitud de los participantes
+    aleatorio = random.randint(0,len(nombres)-1)
+    print('La persona a regalar es ', nombres[aleatorio])
+    
+    #Comprobamos si ya se ha asignado un regalado a esa persona
+    for z in regalos:
+        if z == nombres[aleatorio]:
+            print(z, 'es igual a ', nombres[aleatorio], ', el nombre ya está cogido')
+            repetido = True
+            
+    #Mientras el nombre sea el del usuario o ya se le haya asignado un regalador se repite el random
+    while nombres[aleatorio] == x or repetido:
+        #Generamos un nuevo aleatorio y ponemos el repetido en False para volver a comprobar
+        aleatorio = random.randint(0,len(nombres)-1)
+        repetido = False
+        print('La persona a regalar es ', nombres[aleatorio])
+
+        #Comprobamos si esa persona ya se le ha asignado regalador
+        for z in regalos:
+            if z == nombres[aleatorio]:
+                print(z, 'es igual a ', nombres[aleatorio], 'el nombre ya está cogido')
+                repetido = True
+
+            
+    #Cuando encontramos un nombre adecuado lo agregamos
+    regalos.append(nombres[aleatorio])
+    
+
+
+#Creamos una tabla con las dos listas y lo guardamos en un csv
+tabla = pd.DataFrame()
+tabla ['Regaladores'] = nombres
+tabla ['Regalados'] = regalos
+
+tabla.to_csv('D:/Users/María Rollán/Documents/Programación y frikadas/Amigo invisible/amigo_invisible.csv')
+
+
+
+        
+
---
+++ Saber a quien regalas.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 11 22:33:38 2020
+
+@author: Maria Rollan
+"""
+
+import random
+import numpy as np
+import pandas as pd
+
+
+tabla = pd.read_csv('D:/Users/María Rollán/Documents/Programación y frikadas/Amigo invisible/amigo_invisible.csv')
+
+longitud = len(tabla.index)
+
+print('En este sorteo participan:')
+for j in range (longitud):
+    print(tabla.iloc[j][1])
+    
+print('\n \n Introduce tu nombre para saber a quien regalas')
+tu_nombre = input()
+
+for x in range(longitud): 
+    if tabla.iloc[x][1] == tu_nombre:
+        print('regalas a ', tabla.iloc[x][2])
